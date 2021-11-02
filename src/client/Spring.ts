3785184ca486041d6004f52adda579159d25f03e
jonasnoki <Jonas.Noack@student.hpi.uni-potsdam.de>
2021-11-02 16:56:25 +0100

    spring rendering faster update

--- src/client/Spring.ts
+++ src/client/Spring.ts
@@ -1,76 +1,86 @@
 import {
+    BufferAttribute,
     BufferGeometry,
     Line,
     LineBasicMaterial,
     Material,
-    Vector3
-} from "three";
-import {SpringParticle} from "./SpringParticle";
+    Vector3,
+} from 'three'
+import { SpringParticle } from './SpringParticle'
 
 
 export class Spring {
-    private particleA: SpringParticle;
-    private particleB: SpringParticle;
-    private elasticity: number;
-    private damping: number;
-    private restingDist: number;
-    private mesh: Line;
-    private showSpring: boolean;
+    private particleA: SpringParticle
+    private particleB: SpringParticle
+    private elasticity: number
+    private damping: number
+    private restingDist: number
+    private mesh: Line
+    private showSpring: boolean
+    private positionBuffer: BufferAttribute;
 
-    private geometry = new BufferGeometry();
+    private geometry = new BufferGeometry()
     private static material = new LineBasicMaterial({
-        color: 0xfaf0be
-    });
+        color: 0xfaf0be,
+    })
 
     constructor(particleA: SpringParticle, particleB: SpringParticle, elasticity: number, damping: number, showSpring: boolean) {
-        this.particleA = particleA;
-        this.particleB = particleB;
-        this.elasticity = elasticity;
-        this.damping = damping;
-        this.showSpring = showSpring;
+        this.particleA = particleA
+        this.particleB = particleB
+        this.elasticity = elasticity
+        this.damping = damping
+        this.showSpring = showSpring
         this.restingDist = this.particleA.getCurrentPosition().clone().sub(this.particleB.getCurrentPosition()).length()
-        this.mesh = new Line( this.geometry, Spring.material );
+        this.mesh = new Line(this.geometry, Spring.material)
+        const pA = this.particleA.getCurrentPosition();
+        const pB = this.particleB.getCurrentPosition();
+        this.positionBuffer =  new BufferAttribute(new Float32Array([pA.x, pA.y, pA.z, pB.x, pB.y, pB.z]), 3)
+        this.geometry.setAttribute('position', this.positionBuffer);
+        this.mesh.visible = this.showSpring;
     }
 
-    public setElasticity(e: number){
-        this.elasticity = e;
+    public setElasticity(e: number) {
+        this.elasticity = e
     }
 
-    public setDamping(d: number){
-        this.damping = d;
+    public setDamping(d: number) {
+        this.damping = d
     }
 
-    public setShowSpring(s: boolean){
+    public setShowSpring(s: boolean) {
         this.particleA.getMesh().visible = !s
         this.particleB.getMesh().visible = !s
-        this.showSpring = s;
-        this.mesh.visible = s;
+        this.showSpring = s
+        this.mesh.visible = s
     }
 
     public calcForce(particle: SpringParticle): Vector3 {
-        const pA =  this.particleA.getCurrentPosition().clone();
-        const pB = this.particleB.getCurrentPosition().clone();
+        const pA = this.particleA.getCurrentPosition().clone()
+        const pB = this.particleB.getCurrentPosition().clone()
 
-        if(this.showSpring){
-            this.geometry.setFromPoints([pA, pB]);
+        if (this.showSpring) {
+            this.positionBuffer.setXYZ(0, pA.x, pA.y, pA.z);
+            this.positionBuffer.setXYZ(1, pB.x, pB.y, pB.z);
+            this.positionBuffer.needsUpdate = true;
         }
-        const pbpa = pB.sub(pA);
+        const pbpa = pB.sub(pA)
         const dist = pbpa.length()
-        pbpa.normalize();
-        const vbva: Vector3 = this.particleB.getVelocity().clone().sub(this.particleA.getVelocity());
+        pbpa.normalize()
+        const vbva: Vector3 = this.particleB.getVelocity().clone().sub(this.particleA.getVelocity())
         const force = pbpa.multiplyScalar(this.elasticity * (dist - this.restingDist) + this.damping * vbva.dot(pbpa))
-        return particle === this.particleA ? force : force.negate();
+        return particle === this.particleA ? force : force.negate()
     }
-    
+
     public getMesh(): Line {
-        return this.mesh;
+        return this.mesh
 
     }
+
     public delete() {
-        this.mesh.geometry.dispose();
+        this.mesh.geometry.dispose()
         if (this.mesh.material instanceof Material) {
-            this.mesh.material.dispose();
+            this.mesh.material.dispose()
         }
-        this.mesh.parent?.remove(this.mesh);
+        this.mesh.parent?.remove(this.mesh)
     }
 }
